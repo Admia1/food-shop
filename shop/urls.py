2ce06d6594a98288568c5cdc0c8d6b10816f5568
admia1 <sampadpars@gmail.com>
2020-01-21 10:55:03 +0330

    add login dashboard register view +template

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,8 @@ app_name = "shop"
 
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
 
 def hello(request):
     return HttpResponse("Hello World!")
+
+
+
+def register(request):
+    template='shop/register.html'
+    if not request.method == 'POST':
+        return render(request, template, {})
+
+    #do sth with request.POST['KEY']
+    return render(request, template, {})
+
+
+def login(request):
+    template='shop/login.html'
+    if not request.method == 'POST':
+        return render(request, template, {})
+
+    #do sth with request.POST['KEY']
+    return render(request, template, {})
+
+
+def logout(request):
+    template='shop/logout.html'
+
+    #logout
+
+    return HttpResponseRedirect(reverse('shop:login'))
+
+
+def dashboard(request):
+    template='shop/dashboard.html'
+
+    # if request.POST['user'] not in active_users:
+    #     return HttpResponseRedirect(reverse('shop:login'))
+
+    return render(request, template, {})
