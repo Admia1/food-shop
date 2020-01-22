3e5b34d6932e5960ab67abc637197101e7fee1a3
admia1 <sampadpars@gmail.com>
2020-01-22 07:00:32 +0330

    cookie stuff setup

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.template import loader
+from random import randint
+
+active_cookies = {}#key user_id
+
+def add_cookie(user_id):
+    new_cookie = randint(100000000000000000000000,100000000000000000000000*10-1)
+    while(new_cookie in active_cookies):
+        new_cookie = randint(100000000000000000000000,100000000000000000000000*10-1)
+    active_cookies.update({new_cookie, user_id})
+    print('adddddddddddddddddddddddddddddddddddddddddddddddddddddddddd')
+    return new_cookie
 
 
 def hello(request):
-    return HttpResponse("Hello World!")
+    response = HttpResponse("Hello World!")
+    response.set_cookie('salam','salam')
+    return response
 
 
 
@@ -19,12 +33,23 @@ def register(request):
 
 def login(request):
     template='shop/login.html'
-    if not request.method == 'POST':
+    if request.method != 'POST' or 'username' not in request.POST or 'password' not in request.POST:
         return render(request, template, {})
 
-    #do sth with request.POST['KEY']
-    return render(request, template, {})
 
+    all_users = {('ali','ali')}
+    if (request.POST['username'],request.POST['password']) not in all_users:
+        return render(request, template, {'error_message' : "Wrong username or password"})
+
+    user_id = 1
+
+    t = loader.get_template(template)
+    content = {'logged_in': 1}
+    response = HttpResponse(t.render(content, request))
+    new_cookie = add_cookie(user_id)
+    response.set_cookie('food-shop-cookie',str(new_cookie))
+
+    return response
 
 def logout(request):
     template='shop/logout.html'
@@ -36,8 +61,10 @@ def logout(request):
 
 def dashboard(request):
     template='shop/dashboard.html'
+    print(request.COOKIES)
 
     # if request.POST['user'] not in active_users:
     #     return HttpResponseRedirect(reverse('shop:login'))
 
     return render(request, template, {})
+
