01994cb765e9ba41a24f6458b40564a199ed6f6f
admia1 <sampadpars@gmail.com>
2020-01-23 15:33:13 +0330

    login and register works'

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 app_name = "shop"
 
 urlpatterns = [
-    path('hello/', views.hello, name='hello'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
--- shop/views.py
+++ shop/views.py
@@ -5,66 +5,109 @@ from django.template import loader
 from random import randint
 
 active_cookies = {}#key user_id
+all_users = []
 
 def add_cookie(user_id):
     new_cookie = randint(100000000000000000000000,100000000000000000000000*10-1)
     while(new_cookie in active_cookies):
         new_cookie = randint(100000000000000000000000,100000000000000000000000*10-1)
-    active_cookies.update({new_cookie, user_id})
-    print('adddddddddddddddddddddddddddddddddddddddddddddddddddddddddd')
+    active_cookies.update({str(new_cookie): user_id})
+    print(active_cookies)
     return new_cookie
 
 
-def hello(request):
-    response = HttpResponse("Hello World!")
-    response.set_cookie('salam','salam')
-    return response
-
-
-
 def register(request):
+    if 'food-shop-cookie' in request.COOKIES:
+        if active_cookies.get(request.COOKIES['food-shop-cookie']):
+            return HttpResponseRedirect(reverse('shop:dashboard'))
+
     template='shop/register.html'
     if not request.method == 'POST':
         return render(request, template, {})
 
+    def post_validator(field):
+        if field not in request.POST:
+            return False
+        if not request.POST[field]:
+            return False
+        return True
     #do sth with request.POST['KEY']
-    return render(request, template, {})
+    for field in ['first_name', 'last_name', 'password', 'confirm_password', 'email', 'phone_number']:
+        if not post_validator(field):
+            return render(request, template, {'error_message' : 'invalid post form : '+field})
+
+    for charr in request.POST['phone_number']:
+        if charr not in "1234567890" :
+            return render(request, template, {'error_message' : 'invalid post : phone_number must be integer'})
+
+    if request.POST['password'] != request.POST['confirm_password']:
+        return render(request, template, {'error_message' : 'invalid post : password and confirm_password must be the same'})
+
+    #to do email validator
+    #to do name validator
+    #to do phone number validator
+    #to do password
+
+    for user in all_users:
+        if user['email'] == request.POST['email']:
+            return render(request, template, {'error_message' : 'invalid post : you registered before'})
+
+    new_user_id = randint(10,100000)
+    all_users.append({'id': new_user_id, 'first_name': request.POST['first_name'], 'last_name': request.POST['last_name'], 'email': request.POST['email'], 'password': request.POST['password'], 'phone_number':request.POST['phone_number']})
+
+    t = loader.get_template('shop/login.html')
+    content = {'logged_in': True, 'error_message':'you logged in'}
+    response = HttpResponse(t.render(content, request))
+    new_cookie = add_cookie(new_user_id)
+    response.set_cookie('food-shop-cookie',str(new_cookie))
+
+    return response
 
 
 def login(request):
+    print(all_users)
+    if 'food-shop-cookie' in request.COOKIES:
+        print(request.COOKIES['food-shop-cookie'])
+        if active_cookies.get(request.COOKIES['food-shop-cookie']):
+            return HttpResponseRedirect(reverse('shop:dashboard'))
+
     template='shop/login.html'
-    if request.method != 'POST' or 'username' not in request.POST or 'password' not in request.POST:
+    if request.method != 'POST':
         return render(request, template, {})
+    if ('email' not in request.POST) or ('password' not in request.POST):
+        return render(request, template, {'error_message' : "bad post format", 'logged_in' : False})
 
+    cur_user = False
+    for user in all_users:
+        if user['email'] == request.POST['email'] and user['password'] == request.POST['password']:
+            cur_user = user
+            break
+    if not cur_user:
+        return render(request, template, {'error_message' : "Wrong email or password", 'logged_in' : False})
 
-    all_users = {('ali','ali')}
-    if (request.POST['username'],request.POST['password']) not in all_users:
-        return render(request, template, {'error_message' : "Wrong username or password"})
-
-    user_id = 1
-
-    t = loader.get_template(template)
-    content = {'logged_in': 1}
+    t = loader.get_template('shop/login.html')
+    content = {'logged_in': True, 'error_message':'you logged in'}
     response = HttpResponse(t.render(content, request))
-    new_cookie = add_cookie(user_id)
+    new_cookie = add_cookie(user['id'])
     response.set_cookie('food-shop-cookie',str(new_cookie))
 
     return response
 
 def logout(request):
-    template='shop/logout.html'
-
-    #logout
+    if 'food-shop-cookie' in request.COOKIES:
+        if active_cookies.get(request.COOKIES['food-shop-cookie']):
+            active_cookies.pop(request.COOKIES['food-shop-cookie'])
 
     return HttpResponseRedirect(reverse('shop:login'))
 
 
 def dashboard(request):
-    template='shop/dashboard.html'
-    print(request.COOKIES)
+    if 'food-shop-cookie' not in request.COOKIES:
+        return HttpResponseRedirect(reverse('shop:login'))
+    if not active_cookies.get(request.COOKIES['food-shop-cookie']):
+        return HttpResponseRedirect(reverse('shop:login'))
 
-    # if request.POST['user'] not in active_users:
-    #     return HttpResponseRedirect(reverse('shop:login'))
+    template='shop/dashboard.html'
+    cur_user_id = active_cookies.get(request.COOKIES['food-shop-cookie'])
 
     return render(request, template, {})
-
