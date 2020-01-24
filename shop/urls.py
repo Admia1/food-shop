3ebdf1deb80b5ad796816cfc057b307be183b34f
admia1 <sampadpars@gmail.com>
2020-01-24 21:38:25 +0330

    backup

--- shop/urls.py
+++ shop/urls.py
@@ -9,7 +9,15 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/', views.profile, name='profile'),
+    path('addresses/', views.addresses, name='addresses'),
+    path('address/<int:address_id>/', views.address, name='address'),
+
     path('shops/city/<int:city_id>/', views.shops_city, name='shops_city'),
     path('shops/location/', views.shops_location, name='shops_location'),
-    path('shop/<int:shop_id>/', views.shop, name='shop'),
+    path('shop/<int:shop_id>/', views.shop, name='shop'),    # add and remove food -> do it on shop with post
+
+    path('orders/', views.orders, name='orders'),
+    path('order/<int:shop_id>/', views.order, name='order'),#use post to comment
+
+    #todo search
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,17 +5,17 @@ from django.template import loader
 from random import randint
 
 active_cookies = {}#key user_id
-users = []
-cities = [{'id':1, 'name':'Shiraz'}, {'id':2, 'name':'Tehran'}, {'id':3, 'name':'Isfehan'}, {'id':4, 'name':'Mash-had'}]
+db_users = []
+db_cities = [{'id':1, 'name':'Shiraz'}, {'id':2, 'name':'Tehran'}, {'id':3, 'name':'Isfehan'}, {'id':4, 'name':'Mash-had'}]
 
-shops  = [{'id':2, 'name':'pam-chal', 'about':'no about', 'city_id':1, 'mbv':3000, 'x':100, 'y':100, 'address_text':'end of Afif Abad'},
+db_shops  = [{'id':2, 'name':'pam-chal', 'about':'no about', 'city_id':1, 'mbv':3000, 'x':100, 'y':100, 'address_text':'end of Afif Abad'},
           {'id':3, 'name':'Namak', 'about':'no about', 'city_id':1, 'mbv':13000, 'x':99, 'y':100, 'address_text':'middle of Afif Abad'},
           {'id':4, 'name':'Narvan', 'about':'no about', 'city_id':1, 'mbv':15000, 'x':101, 'y':100, 'address_text':'end of Afif Abad'},
           {'id':5, 'name':'pam-chal Tehran', 'about':'no about', 'city_id':2, 'mbv':3000, 'x':1000, 'y':2000, 'address_text':'Afif Abad of Tehran'},
           {'id':6, 'name':'pam-chal Isfehan', 'about':'no about', 'city_id':3, 'mbv':5000, 'x':2000, 'y':3000, 'address_text':'Afif Abad of Isfehan'},
           {'id':7, 'name':'pam-chal Mash-had', 'about':'no about', 'city_id':4, 'mbv':2000, 'x':-1000, 'y':5000, 'address_text':'Afif Abad of Mash-had'},
         ]
-foods  = [{'id':2, 'name':'pizza', 'price':20000, 'discounted_price':19000, 'discount':30, 'about':'circle food', 'shop_id':1, 'cat_id':4},
+db_foods  = [{'id':2, 'name':'pizza', 'price':20000, 'discounted_price':19000, 'discount':30, 'about':'circle food', 'shop_id':1, 'cat_id':4},
           {'id':3, 'name':'burger', 'price':21000, 'discounted_price':15500, 'discount':30, 'about':'made by sponge bob', 'shop_id':1, 'cat_id':2},
           {'id':4, 'name':'chips', 'price':22000, 'discounted_price':14000, 'discount':30, 'about':'circle food', 'shop_id':1, 'cat_id':3},
           {'id':5, 'name':'veg pizza', 'price':23000, 'discounted_price':21500, 'discount':30, 'about':'circle food', 'shop_id':1, 'cat_id':4},
@@ -30,12 +30,13 @@ foods  = [{'id':2, 'name':'pizza', 'price':20000, 'discounted_price':19000, 'dis
           {'id':14, 'name':'hotdog of Isfehan', 'price':14000, 'discounted_price':9500, 'discount':15, 'about':'long long away', 'shop_id':6, 'cat_id':1},
           {'id':15, 'name':'hotdog of Mash-had', 'price':15000, 'discounted_price':1400, 'discount':10, 'about':'long long away', 'shop_id':7, 'cat_id':1},
         ]
-cat = [{'id':1, 'name':'hotdog'},
+db_cat = [{'id':1, 'name':'hotdog'},
        {'id':2, 'name':'burger'},
        {'id':3, 'name':'potato'},
        {'id':4, 'name':'pizza'},
        {'id':5, 'name':'soup'}
     ]
+db_addresses = [] #{'id':1,'text':'here', 'city_id':1,'user_id':1, 'x':30, 'y':50}
 def add_cookie(user_id):
     new_cookie = randint(100000000000000000000000,100000000000000000000000*10-1)
     while(new_cookie in active_cookies):
@@ -78,12 +79,12 @@ def register(request):
     #to do phone number validator
     #to do password
 
-    for user in users:
+    for user in db_users:
         if user['email'] == request.POST['email']:
             return render(request, template, {'error_message' : 'invalid post : you registered before'})
 
-    new_user_id = randint(10,100000)
-    users.append({'id': new_user_id, 'first_name': request.POST['first_name'], 'last_name': request.POST['last_name'], 'email': request.POST['email'], 'password': request.POST['password'], 'phone_number':request.POST['phone_number']})
+    new_user_id = db_user
+    db_users.append({'id': new_user_id, 'first_name': request.POST['first_name'], 'last_name': request.POST['last_name'], 'email': request.POST['email'], 'password': request.POST['password'], 'phone_number':request.POST['phone_number']})
 
     t = loader.get_template('shop/login.html')
     content = {'logged_in': True, 'error_message':'you logged in'}
@@ -95,7 +96,7 @@ def register(request):
 
 
 def login(request):
-    print(users)
+    print(db_users)
     if 'food-shop-cookie' in request.COOKIES:
         print(request.COOKIES['food-shop-cookie'])
         if active_cookies.get(request.COOKIES['food-shop-cookie']):
@@ -108,7 +109,7 @@ def login(request):
         return render(request, template, {'error_message' : "bad post format", 'logged_in' : False})
 
     cur_user = False
-    for user in users:
+    for user in db_users:
         if user['email'] == request.POST['email'] and user['password'] == request.POST['password']:
             cur_user = user
             break
@@ -139,7 +140,7 @@ def profile(request):
 
     cur_user_id = active_cookies.get(request.COOKIES['food-shop-cookie'])
     cur_user = False
-    for user in users:
+    for user in db_users:
         if user['id'] == cur_user_id:
             cur_user = user
             break
@@ -173,20 +174,20 @@ def dashboard(request):
     template='shop/dashboard.html'
     cur_user_id = active_cookies.get(request.COOKIES['food-shop-cookie'])
     cur_user = False
-    for user in users:
+    for user in db_users:
         if user['id'] == cur_user_id:
             cur_user = user
             break
 
     if not cur_user:
-        return render(request, template, {'error_message':'some thing wrong with cookies', 'cities':cities})
+        return render(request, template, {'error_message':'some thing wrong with cookies', 'cities':db_cities})
 
-    return render(request, template, {'cities':cities, 'user':cur_user})
+    return render(request, template, {'cities':db_cities, 'user':cur_user})
 
 
 def shops_city(request, city_id):
     cur_city = False
-    for city in cities:
+    for city in db_cities:
         if city['id'] == city_id:
             cur_city = city
             break
@@ -195,7 +196,7 @@ def shops_city(request, city_id):
         return render(request, 'shop/shops.html', {'error_message':'invalid city id'})
 
     city_shops = []
-    for shop in shops:
+    for shop in db_shops:
         print(shop)
         if shop['city_id'] == cur_city['id']:
             city_shops.append(shop)
@@ -212,15 +213,14 @@ def shops_location(request):
         return render(request, 'shop/shops.html', {'error_message':'invalid format geo_x geo_y'})
 
     near_shops=[]
-    for shop in shops:
+    for shop in db_shops:
         if ( (shop['x']-geo_x)**2 + (shop['y']-geo_y)**2 ) <= 50*2:
             near_shops.append(shop)
     return render(request, 'shop/shops.html', {'shops':near_shops})
 
 def shop(request, shop_id):
-
     cur_shop = False
-    for shop in shops:
+    for shop in db_shops:
         if shop['id'] == shop_id:
             cur_shop = shop
             break
@@ -229,8 +229,136 @@ def shop(request, shop_id):
         return render(request, 'shop/shop.html', {'error_message':'wrong shop id'})
 
     shop_foods=[]
-    for food in foods:
+    for food in db_foods:
         if food['shop_id'] == shop_id:
             shop_foods.append(food)
     #shop_foods -> with discounted_price
-    return render(request, 'shop/shop.html', {'shop':cur_shop, 'foods':shop_foods})
+    if request.method != 'POST':
+        return render(request, 'shop/shop.html', {'shop':cur_shop, 'foods':shop_foods})
+    #add or remove item
+    #TODO
+
+
+
+def addresses(request):
+    template = 'shop/addresses.html'
+    if 'food-shop-cookie' not in request.COOKIES:
+        return HttpResponseRedirect(reverse('shop:login'))
+    if not active_cookies.get(request.COOKIES['food-shop-cookie']):
+        return HttpResponseRedirect(reverse('shop:login'))
+
+    cur_user_id = active_cookies.get(request.COOKIES['food-shop-cookie'])
+    cur_user = False
+    for user in db_users:
+        if user['id'] == cur_user_id:
+            cur_user = user
+            break
+    if not cur_user:
+        return render(request, template, {'error_message': 'my bad'})
+
+    user_addresses = []
+    for address in db_addresses:
+        if address['user_id'] == cur_user_id:
+            user_addresses.append(address)
+
+    if request.method!='POST':
+        return render(request, template, {'addresses':user_addresses})
+
+    for field in ['city_id', 'text', 'geo_x', 'geo_y']:
+        if not post_validator(request,field):
+            return render(request, template, {'error_message' : 'invalid post form : '+field, 'addresses':cur_user_addresses})
+    try:
+        geo_x=float(request.POST['geo_x'])
+        geo_y=float(request.POST['geo_y'])
+    except:
+        return render(request, template, {'error_message':'geo_x geo_y bad format'})
+    new_address = {'id':len(db_addresses)+1 ,'user_id':cur_user_id, 'city_id': int(request.POST['city_id']), 'text':request.POST['text'], 'geo_x':geo_x, 'geo_y':geo_y}
+    db_addresses.append(new_address)
+    user_addresses.append(new_address)
+    return render(request, template, {'user_id':cur_user, 'addresses':user_addresses})
+
+
+def address(request, address_id):
+    template = 'shop/address.html'
+    if 'food-shop-cookie' not in request.COOKIES:
+        return HttpResponseRedirect(reverse('shop:login'))
+    if not active_cookies.get(request.COOKIES['food-shop-cookie']):
+        return HttpResponseRedirect(reverse('shop:login'))
+
+    cur_user_id = active_cookies.get(request.COOKIES['food-shop-cookie'])
+    cur_user = False
+    for user in db_users:
+        if user['id'] == cur_user_id:
+            cur_user = user
+            break
+    if not cur_user:
+        return render(request, template, {'error_message': 'my bad'})
+
+    cur_address = False
+    for address in db_addresses:
+        if address['id'] == address_id:
+            cur_address = address
+    if not cur_address:
+        return render(request, template, {'error_message':'wrong address id'})
+    if cur_address['user_id'] != cur_user_id:
+        return render(request, template, {'error_message':'this address don\'t belong to you!'})
+
+    if request.method!='POST':
+        print(cur_address)
+        return render(request, template, {'address':cur_address})
+
+
+    for field in ['city_id', 'text', 'geo_x', 'geo_y']:
+        if not post_validator(request,field):
+            return render(request, template, {'error_message' : 'invalid post form : '+field, 'address':cur_address})
+    try:
+        geo_x=float(request.POST['geo_x'])
+        geo_y=float(request.POST['geo_y'])
+    except:
+        return render(request, template, {'error_message':'geo_x geo_y bad format'})
+    new_address = {'id':cur_address['id'] , 'user_id':cur_user_id, 'city_id': int(request.POST['city_id']), 'text':request.POST['text'], 'geo_x':geo_x, 'geo_y':geo_y}
+    #change address
+    for x in cur_address:
+        cur_address[x]=new_address[x]
+    return render(request, template, {'address': cur_address})
+
+def order(request, order_id):
+    template = 'shop/order.html'
+    if 'food-shop-cookie' not in request.COOKIES:
+        return HttpResponseRedirect(reverse('shop:login'))
+    if not active_cookies.get(request.COOKIES['food-shop-cookie']):
+        return HttpResponseRedirect(reverse('shop:login'))
+
+    cur_user_id = active_cookies.get(request.COOKIES['food-shop-cookie'])
+    cur_user = False
+    for user in db_users:
+        if user['id'] == cur_user_id:
+            cur_user = user
+            break
+    if not cur_user:
+        return render(request, template, {'error_message': 'my bad'})
+
+    cur_order = False
+    for order in db_orders:
+        if order['user_id'] == cur_user_id:
+            cur_order = order
+    if not cur_order:
+        return render(request, template, {'error_message':'wrong order id'})
+    if cur_order['user_id'] != cur_user_id:
+        return render(request, template, {'error_message':'this order don\'t belong to you!'})
+
+    if request.method!='POST':
+        return render(request, template, {'order':order})
+    #else do comment job
+
+    for field in ['rate', 'text']:
+        if not post_validator(request,field):
+            return render(request, template, {'error_message' : 'invalid post form : '+field, 'order':order})
+
+    
+
+
+
+
+def orders(request):
+    pass
